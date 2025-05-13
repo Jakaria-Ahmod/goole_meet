32738a853dda3502f64aeee5c6889b3816fe7dba
jakariaahmod <jakariaahmodmd@gmail.com>
2025-05-13 13:22:11 +0600

    sucess

--- src/components/Coll.jsx
+++ src/components/Coll.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { data, useParams } from 'react-router';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 const Coll = () => {
+  const { collId } = useParams();
+  const meet = element => {
+    const appID = 122576635;
+    const serverSecret = 'bcdadbe62f6eca739368a5e34ecd1ebe';
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      collId,
+      Date.now().toString(),
+      'Devoloper name jakaria'
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.VideoConference,
+      },
+    });
+  };
+
   return (
     <div>
-      <h1> iam coll </h1>
+      <div
+        className="myCallContainer"
+        ref={meet}
+        style={{ width: '100vw', height: '100vh' }}
+      ></div>
     </div>
   );
 };
